e4c6ed1b698f472bc51ec06ce1e8f20a8a8f6064
senafathoni2998 <senaahmad2998@gmail.com>
2026-01-25 00:31:43 +0700

    feat: add @vercel/speed-insights for performance monitoring

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,13 @@ import { RouterProvider } from "react-router";
 import router from "./router/route";
 import { Provider } from "react-redux";
 import store from "./app/store";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <SpeedInsights />
     <Provider store={store}>
       <RouterProvider router={router} />
     </Provider>
-  </StrictMode>
+  </StrictMode>,
 );
