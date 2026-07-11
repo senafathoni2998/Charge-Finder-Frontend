58029941b0b7c05949f3f442ca4af8e2467ae0c4
senafathoni2998 <senaahmad2998@gmail.com>
2026-07-11 21:42:11 +0700

    feat(reviews): export StarRating from the components barrel

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export * from './SEO'
 export { default as LanguageSwitcher } from './LanguageSwitcher'
 export { default as LanguageCard } from './LanguageCard'
+export { default as StarRating } from './StarRating'
